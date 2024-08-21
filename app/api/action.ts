edd2409447a4d795d01f1c23e7a608395afb802c
AlmamyHaidara <almamyh27@gmail.com>
2024-08-21 10:20:48 +0000

    client stricture review

--- app/api/action.ts
+++ app/api/action.ts
@@ -1,21 +1,21 @@
 "use server"
 import { PrismaClient } from '@prisma/client';
-import { InvalidateQueryFilters, useMutation, useQueries, useQuery, useQueryClient } from '@tanstack/react-query';
+// import { InvalidateQueryFilters, useMutation, useQueries, useQuery, useQueryClient } from '@tanstack/react-query';
 
 
 const prisma = new PrismaClient()
-export const findTypeDocument= async()=>{
-    // const queryClient = useQueryClient();
+// export const findTypeDocument= async()=>{
+//     // const queryClient = useQueryClient();
 
-    const {  isError, data, error } = useQuery({ queryKey: ['typeDocument'], queryFn:  fetchTypeDocument });
-    if(isError){
-        console.log(error);
+//     const {  isError, data, error } = useQuery({ queryKey: ['typeDocument'], queryFn:  fetchTypeDocument });
+//     if(isError){
+//         console.log(error);
 
-        return null
-    }
+//         return null
+//     }
 
-    return data
-}
+//     return data
+// }
 export const fetchTypeDocument = async () => {
    const data =  await prisma.typesDocuments.findMany({
         include: {
