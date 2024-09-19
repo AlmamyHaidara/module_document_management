4117f7bc54b9a1523d11ae129a362c97b5722620
almamysamina <almamy.haidara@samina.tech>
2024-09-19 12:55:53 +0000

    crud

--- app/components/DossierTable.tsx
+++ app/components/DossierTable.tsx
@@ -20,6 +20,7 @@ import { DossierContext } from '../(main)/uikit/table/DossierExpandTable';
 import { Image } from 'primereact/image';
 import UploadFileComponent from './UploadFileComponent';
 import { fi } from '@faker-js/faker';
+import { Divider } from 'primereact/divider';
 
 type PropsType = {
     dossiers: any[];
@@ -38,7 +39,7 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
     const [submitted, setSubmitted] = useState(false);
     const [nom, setNom] = useState('');
     const [description, setDescription] = useState('');
-    const [docType, setDocType] = useState<{ id: number; code: string; nom_type: string; piece?: any[] }>({ id: 0, code: '', nom_type: '' });
+    const [docType, setDocType] = useState<{ id: number; code: string; nom_type: string; piece?: any[] } | any>({ id: 0, code: '', nom_type: '' });
     const [compteClient, setCompteClient] = useState({ id: 0, code: '', nom_type: '' });
     const [docTypeMeta, setDocTypeMeta] = useState([]);
     const [activeStep, setActiveStep] = useState(0);
@@ -48,6 +49,9 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
     const [fileUploaded, setFileUploaded] = useState<any>([]);
     const [deleteDossierDialog, setDeleteDossierDialog] = useState(false);
     const [editAction, setEditAction] = useState(false);
+    const [dossierInfo, setDossierInfo] = useState([]);
+    const [viewClientsDialog, setViewClientsDialog] = useState(false);
+
     const toast = useRef<any>(null);
     const dt = useRef<any>(null);
 
@@ -64,8 +68,8 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
     }, [filePaths]);
 
     useEffect(() => {
-        setPieces( docType?.piece)
-        console.log("Is rerended",pieces);
+        setPieces(docType?.piece);
+        console.log('Is rerended', pieces);
     }, [docType]);
 
     const onGlobalFilterChange = (e: any) => {
@@ -92,6 +96,10 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
         setDeleteDossierDialog(false);
     };
 
+    const hideDeleteViewClientDialog = () => {
+        setViewClientsDialog(false);
+    };
+
     const saveDossier = () => {
         setSubmitted(true);
         console.log('-------------FormValues', formValues);
@@ -112,7 +120,7 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
                 compteClient: compteClient
             };
 
-            console.log("--------------NewDossier: ", newDossier)
+            console.log('--------------NewDossier: ', newDossier);
             onCreateDossier(newDossier);
             setDossierDialog(false);
             setDossier(null);
@@ -177,7 +185,6 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
             setDocType(newDoctype);
         }
 
-
         console.log('-------------155document: ', docType);
 
         setFormValues(document.dossierInfos || []);
@@ -185,6 +192,17 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
         setActiveStep(0);
         setDossierDialog(true);
         setEditAction(true);
+    };
+
+    const viewClientInfo = async (rowData: any) => {
+        console.log('ewsdcx ', rowData);
+        setDossier({ ...rowData });
+       setDossierInfo(rowData?.dossierInfos);
+        setPieces(rowData?.dossiers_typesDocuments[0]?.dossier?.piece);
+      
+        // console.log("ppppppppppppppppppppppp",rowData);
+
+        setViewClientsDialog(true);
     };
 
     const handleFieldChange = (id: number, cle: string, value: string) => {
@@ -226,6 +244,7 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
     const actionBodyTemplate = (rowData: any) => {
         return (
             <>
+                <Button icon="pi pi-eye" rounded severity="info" className="mr-2" onClick={() => viewClientInfo(rowData)} />
                 <Button icon="pi pi-pencil" rounded severity="success" className="mr-2" onClick={() => editDossier(rowData)} />
                 <Button icon="pi pi-trash" rounded severity="warning" onClick={() => confirmDeleteDossier(rowData)} />
             </>
@@ -318,7 +337,8 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
                                 <div className="field">
                                     <label htmlFor="compteClient">Compte client</label>
                                     <DropDownComponent options={compteMatricule} setTypeDocument={setCompteClient} typeDocument={compteClient} placeholder={'Selectionne un compte client'} />
-                                    {submitted && !compteClient?.id && <small className="p-invalid">Le compte client est requis.</small>}
+                                    {/* {submitted && !comptedossier?.id && <small className="p-invalid">Le compte client est requis.</small>} */}
+                                    client?{' '}
                                 </div>
                             </>
                         ) : activeStep === 1 ? (
@@ -331,34 +351,16 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
                             <>
                                 {docType?.piece && docType?.piece.map((item: any, index: number) => <UploadFileComponent piece={item} setFilePaths={setFilePaths} key={index} />)}
 
-                               { editAction && <div className="w-full h-200 flex justify-between align-center">
-                                   {JSON.stringify(pieces)}
-                                    {
-                                        pieces.map((item:any,index)=>(
-
-                                            <div className=" w-full h-full object-cover" key={index} >
-                                                 <Image src={item?.path} alt="Image" className="w-full h-full object-fill" />
+                                {editAction && (
+                                    <div className="w-full h-200 flex justify-between align-center">
+                                        {JSON.stringify(pieces)}
+                                        {pieces?.map((item: any, index) => (
+                                            <div className=" w-full h-full object-cover" key={index}>
+                                                <Image src={item?.path} alt="Image" className="w-full h-full object-fill" />
                                             </div>
-
-                                        ))
-                                    }
-                                </div>}
-                                {/* <UploadMultipleFile setFilePaths={setFilePaths} /> */}
-
-                                {/* {
-                                editAction && <div className="w-full h-200 flex justify-between align-center">
-                                    {
-                                        pieces.map((item:any,index)=>(
-
-                                            <div className=" w-full h-full object-cover" key={index} >
-                                                 <Image src={item?.path} alt="Image" className="w-full h-full object-fill" />
-                                            </div>
-
-                                        ))
-                                    }
-                                </div>
-                               } */}
-                                {/* </div> */}
+                                        ))}
+                                    </div>
+                                )}
                             </>
                         )}
                     </Dialog>
@@ -384,6 +386,49 @@ const DossierTable = ({ dossiers, globalFilterValue, setGlobalFilterValue, onUpd
                                 </span>
                             )}
                         </div>
+                    </Dialog>
+
+                    <Dialog visible={viewClientsDialog} style={{ width: '70%' }} header={`Les information sur du ${dossier?.nom}`} modal  onHide={hideDeleteViewClientDialog}>
+                        <div className="confirmation-content align-left">
+                            <div className="flex  text-center">
+                                <p className="text-xl">
+                                    <span className="text-2xl font-medium">Code client:</span> {dossier?.code}
+                                </p>
+                            </div>
+                            <div className="flex  text-center">
+                                <p className="text-xl">
+                                    <span className="text-2xl text-left font-medium">Nom:</span> {dossier?.nom}
+                                    
+                                </p>
+                            </div>
+
+                            <Divider />
+                            <p className="text-2xl font-light mt-2 ">La description du dossier </p>
+                            {dossierInfo?.map((res:any, index) => (
+                                <div className="flex  text-center" key={index}>
+                                    <p className="text-xl">
+                                        <span className="text-2xl text-left font-medium">{res?.cle} :</span>
+                                        {' ' + res?.value}
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                        <Divider />
+
+                        <p className="text-2xl font-light ">Piece jointe</p>
+                        <div className="card flex justify-content-center">
+                            {
+                                pieces.map((item: any, index) => (
+                                    <div className="m-5" key={index}>
+                                        <p className="text-2xl font-light ">{item?.nom}</p>
+
+                                        <Image src={item?.path} indicatorIcon={<i className="pi pi-search"></i>} alt="Image" preview className="object-fill" width="100" height="100" />
+                                    </div>
+
+                                ))}
+                        </div>
+
+                       
                     </Dialog>
                 </div>
             </div>
